54125f84cd9902da2d48113806269c69c4ef1a32
xucl <xucl@Ctrip.com>
2016-08-05 12:25:05 +0800

    更新readme.md

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+
 # 简介
 
-这里存放着本人平时以思维导图形式整理的相关资料信息，如有引用和转载，请注明出处：[https://github.com/xucongli1989/MyMindMap](https://github.com/xucongli1989/MyMindMap)
+这里存放着本人平时以思维导图形式整理的相关资料信息，如有引用和转载，请注明出处。
+
+# 目录
+
+## git ##
+
+Git.xmind
+
+## javascript ##
+
+ECMAScript6.xmind
+
+__proto__与prototype.xmind
+
+高性能javascript.xmind
+
+----------
+本目录更新时间：Fri Aug 05 2016 12:24:29 GMT+0800 (中国标准时间)
---
+++ index.js
@@ -0,0 +1,42 @@
+var fs=require('fs');
+var fsi = require('fs-filesysteminfo');
+var rootDirs=['git','javascript'];
+var bookItems=[];
+var tempStr='';
+var dtNow=new Date();
+
+var getFileList=function(rootPath){
+    var args=arguments;
+    var fsiObj = new fsi.FileInfo(rootPath);
+    if(fsiObj.flags.isDirectory){
+        //tempStr+='—';
+        bookItems.push('## '+tempStr+fsiObj.name+' ##');
+        fs.readdirSync(rootPath).forEach(file=>{
+            args.callee(rootPath+'/'+file);
+        });
+        //tempStr=tempStr.substr(1);
+    }else{
+        bookItems.push(fsiObj.name);
+    }
+};
+
+fs.readdirSync('./').forEach(file=>{
+    if(rootDirs.indexOf(file)==-1) return;
+    getFileList(file);
+});
+
+//更新readme.md
+fs.writeFile('./readme.md', `
+# 简介
+
+这里存放着本人平时以思维导图形式整理的相关资料信息，如有引用和转载，请注明出处。
+
+# 目录
+
+${bookItems.join('\r\n\r\n')}
+
+----------
+本目录更新时间：${dtNow}
+`, null, function () {
+    console.log('updated readme.md')
+});
