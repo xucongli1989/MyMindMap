c6841c07690c3b9407461008b14afaeabc597237
xucl <xucl@Ctrip.com>
2016-08-08 14:57:18 +0800

    更新readme.md

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ fs.writeFile('./readme.md', `
 
 这里存放着本人平时以思维导图形式整理的相关资料信息，如有引用和转载，请注明出处。
 
+# XMind
+
+需要安装免费的[XMind思维导图软件](http://www.xmindchina.net/)，才能打开相应的文件。
+
+----------
+
 # 目录
 
 ${bookItems.join('\r\n\r\n')}
